4930680fddfe92b729525f5b9c9d1f8b25c2ef88
Alexander Ziskind <alex@nuvious.com>
2018-01-07 12:06:54 -0500

    fixed missing chore item type

--- app/core/constants/pt-item-types.ts
+++ app/core/constants/pt-item-types.ts
@@ -10,7 +10,7 @@ const PT_ITEM_TYPE_IMPEDIMENT: PtItemType = 'Impediment';
 export class ItemType {
     private static itPbi = new ItemType(PT_ITEM_TYPE_PBI);
     private static itBug = new ItemType(PT_ITEM_TYPE_BUG);
-    private static itChore = new ItemType(PT_ITEM_TYPE_BUG);
+    private static itChore = new ItemType(PT_ITEM_TYPE_CHORE);
     private static itImpediment = new ItemType(PT_ITEM_TYPE_IMPEDIMENT);
 
     public static get Pbi() {
