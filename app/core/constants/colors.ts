494ae5dd16ab7eaf179b8c4ae7c49453e3fe03e9
Alexander Ziskind <alex@nuvious.com>
2018-01-07 11:54:34 -0500

    changed color constants

--- app/core/constants/colors.ts
+++ app/core/constants/colors.ts
@@ -1,5 +1,5 @@
 import { Color } from 'color';
 
-export const COLOR_LIGHT = new Color('#CDDC39');
-export const COLOR_DARK = new Color('#4CAF50');
+export const COLOR_LIGHT = new Color('#b2b4d3');
+export const COLOR_DARK = new Color('#555a97');
 export const COLOR_GRAY = new Color('#F9F9F9');
