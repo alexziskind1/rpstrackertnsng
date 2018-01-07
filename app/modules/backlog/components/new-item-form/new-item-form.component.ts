38515a76b34f4761014ef38a65d3a584a354ff5f
Alexander Ziskind <alex@nuvious.com>
2018-01-07 12:07:16 -0500

    refactored new form model and ui

--- app/modules/backlog/components/new-item-form/new-item-form.component.ts
+++ app/modules/backlog/components/new-item-form/new-item-form.component.ts
@@ -1,10 +1,16 @@
 import { Component, Input, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
+import { android as androidApplication } from 'application';
 import { RadDataFormComponent } from 'nativescript-pro-ui/dataform/angular';
+import { DataFormEventData } from 'nativescript-pro-ui/dataform';
 
-import { PtNewItemForm } from '../../../../shared/models/forms/pt-new-item-form.model';
+import { PtItemType } from '../../../../core/models/domain/types';
+import { PtNewItemForm, initializeNewItemForm } from '../../../../shared/models/forms/pt-new-item-form.model';
 import { ItemType } from '../../../../core/constants/pt-item-types';
 import { EMPTY_STRING } from '../../../../core/helpers/string-helpers';
+import { setMultiLineEditorFontSize, setPickerEditorImageLocation } from '../../../../shared/helpers/ui-data-form';
+
+
 
 
 @Component({
@@ -25,14 +31,45 @@ export class NewItemFormComponent implements OnInit {
     public newItemForm: PtNewItemForm;
     public itemTypesProvider = ItemType.List.map((t) => t.PtItemType);
 
+    private selectedTypeValue: PtItemType;
+
+    public get itemTypeImage() {
+        return ItemType.imageResFromType(this.selectedTypeValue);
+    }
+
+    public get itemTypeEditorDisplayName() {
+        return 'Type';
+    }
+
     constructor() { }
 
     public ngOnInit() {
-        this.newItemForm = {
-            title: EMPTY_STRING,
-            description: EMPTY_STRING,
-            typeStr: EMPTY_STRING
-        };
+        this.newItemForm = initializeNewItemForm();
+    }
+
+    public onEditorUpdate(args: DataFormEventData) {
+        if (androidApplication) {
+            /*
+            switch (args.propertyName) {
+
+            }
+            */
+        } else {
+            switch (args.propertyName) {
+                case 'title': this.editorSetupMultiLineEditorIos(args.editor); break;
+                case 'description': this.editorSetupMultiLineEditorIos(args.editor); break;
+                case 'typeStr': this.editorSetupTypeEditorIos(args.editor); break;
+            }
+        }
+    }
+
+    private editorSetupMultiLineEditorIos(editor) {
+        setMultiLineEditorFontSize(editor, 17);
+    }
+
+    private editorSetupTypeEditorIos(editor) {
+        setPickerEditorImageLocation(editor);
+        this.selectedTypeValue = editor.editorValueLabel.text;
     }
 
     public onSaveTap() {
