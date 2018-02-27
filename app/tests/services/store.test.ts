1fd3e35b137d28686e3200d15f5ea6fd6ec2f0a3
Alexander Ziskind <alex@nuvious.com>
2018-02-27 12:23:12 -0500

    store test removed unused tests

--- app/tests/services/store.test.ts
+++ app/tests/services/store.test.ts
@@ -8,32 +8,5 @@ describe('Make sure store works', () => {
         const store = new Store();
         store.set('selectedPreset', 'open');
         expect(store.value.selectedPreset).toBe('open');
-
-    });
-
-    it('must work2', () => {
-        const store = new Store();
-        store.set('selectedPreset', 'open');
-        expect(store.value.selectedPreset).toBe('open');
-
-    });
-});
-
-
-describe('Make sure store works 2', () => {
-    it('must work', () => {
-        const store = new Store();
-        store.set('selectedPreset', 'open');
-        expect(store.value.selectedPreset).toBe('open');
-
-    });
-
-    it('must work2', () => {
-        const store = new Store();
-        store.set('selectedPreset', 'open');
-        expect(store.value.selectedPreset).toBe('open');
-
-        store.set('selectedPreset', 'doodoo');
-        expect(store.value.selectedPreset).toBe('open');
     });
 });
